483b5ecd99130b18161b7cfd82f574213bd23dca
kim7881 <rlawls7881@gmail.com>
2022-08-03 18:04:21 +0900

    calendar

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,97 @@
+import 'package:calendar_scheduler/component/calendar.dart';
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  DateTime focusedDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: renderFloatingActionButton(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Calendar(
+              selectedDay: selectedDay,
+              focusedDay: focusedDay,
+              onDaySelected: onDaySelected,
+            ),
+            SizedBox(height: 8.0),
+            TodayBanner(
+              selectedDay: selectedDay,
+              scheduleCount: 3,
+            ),
+            SizedBox(height: 8.0),
+            _ScheduleList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  FloatingActionButton renderFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          // isScrollController : 키패드 올라간 위치만큼 올라감
+          isScrollControlled: true,
+          builder: (_){
+            return ScheduleBottomSheet();
+          },
+        );
+      },
+      child: Icon(Icons.add),
+    );
+  }
+
+  onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+      this.focusedDay = selectedDay;
+    });
+  }
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: ListView.separated(
+          itemCount: 10,
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 8.0);
+          },
+          itemBuilder: (context, index) {
+            return ScheduleCard(
+              startTime: 8,
+              endTime: 9,
+              content: '프로그래밍 공부하기',
+              color: Colors.red,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
