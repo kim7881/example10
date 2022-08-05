e81eab2f864cb625110977284848513c0d67951b
kim7881 <rlawls7881@gmail.com>
2022-08-05 09:25:48 +0900

    Hi

---
+++ lib/database/drift_database.dart
@@ -0,0 +1,53 @@
+// private 값들은 불러올 수 없다.
+import 'dart:io';
+
+import 'package:calendar_scheduler/model/category_color.dart';
+import 'package:calendar_scheduler/model/schedule.dart';
+// import 'package:calendar_scheduler/model/schedule_with_color.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+// private 값까지 불러올 수 있다.
+part 'drift_database.g.dart';
+
+@DriftDatabase(
+  tables: [
+    Schedules,
+    CategoryColors,
+  ],
+)
+class LocalDatabase extends _$LocalDatabase {
+  LocalDatabase() : super(_openConnection());
+
+  Future<Schedule> getScheduleById(int id) =>
+      (select(schedules)..where((tbl) => tbl.id.equals(id))).getSingle();
+
+  Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+
+  Future<int> createCategoryColor(CategoryColorsCompanion data) =>
+      into(categoryColors).insert(data);
+
+  Future<List<CategoryColor>> getCategoryColors() =>
+      select(categoryColors).get();
+
+  Future<int> updateScheduleById(int id, SchedulesCompanion data) =>
+      (update(schedules)..where((tbl) => tbl.id.equals(id))).write(data);
+
+  Future<int> removeSchedule(int id) =>
+      (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
